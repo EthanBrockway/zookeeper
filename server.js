ec58444a858c12d2e8433eba3665d03267cac7c7
Skronkie <ebrockway01@gmail.com>
2022-03-08 15:24:12 -0600

    added id param and error message for 404 page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { query } = require("express");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const { animals } = require("./data/animals");
@@ -8,12 +9,25 @@ app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.status(404).send("Nah dog this page isnt working :/");
+  }
 });
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
